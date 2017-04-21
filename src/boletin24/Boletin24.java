b708d7e4785687518aa4c00c707addda08f61fc0
ggarciamartinez <ggarciamartinez@atanasoff20.danielcastelao.org>
2017-04-21 12:36:26 +0200

    comm1

---
+++ src/boletin24/Boletin24.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin24;
+
+/**
+ *
+ * @author ggarciamartinez
+ */
+public class Boletin24 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        EJ1 v1 = new EJ1();
+        EJ2 v2 = new EJ2();
+        v1.setVisible(true);
+        v2.setVisible(true);
+    }
+    
+}
